1d1e10c72d5ca83c44b4bd800f6c73d11669a1ed
nurozkannn <ozkanhalime42@gmail.com>
2024-08-14 10:56:55 +0300

    Simon game

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,34 @@
+
+buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+userClickedPattern = [];
+
+$(".btn").on("click",function () {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+      }, 100);
+}
